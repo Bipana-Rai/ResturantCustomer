4fdbfffb99862abd54d1072bdbc28c67a8ae9e71
Bipana-Rai <bipanarai982@gmail.com>
2025-05-10 19:31:37 +0545

    r

---
+++ src/components/TableCard.tsx
@@ -0,0 +1,17 @@
+
+import { LiaChairSolid } from "react-icons/lia";
+function TableCard() {
+  return (
+    <>
+    <div className=' relative h-[200px] w-[300px] bg-amber-400'>
+        <div className="h-[90px] w-[180px] bg-amber-50 absolute top-13 left-15 rounded-2xl flex items-center justify-center">
+            table 1
+        </div>
+        <LiaChairSolid />
+
+    </div>
+    </>
+  )
+}
+
+export default TableCard
