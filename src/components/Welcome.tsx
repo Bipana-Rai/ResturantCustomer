a035a2c618b2d9673be3056cee2163e2bfc59555
Bipana-Rai <bipanarai982@gmail.com>
2025-05-18 18:33:44 +0545

    v

--- src/components/Welcome.tsx
+++ src/components/Welcome.tsx
@@ -1,12 +1,17 @@
 
-
-function Welcome() {
+interface authenticationProps{
+  showLogin:boolean
+}
+function Welcome({showLogin}:authenticationProps) {
   return (
     <>
-      <img src="/log.webp" alt="" className=" absolute h-full w-full " />
-      <div className="h-full px-4 flex flex-col items-center justify-center absolute w-full bg-[#00000079] z-20 ">
-        <p className="text-2xl font-bold text-white"> Welcome Back</p>
-        <p className="text-white font-semibold text-center py-3">Please log in using your personal information to stay connected with us</p>
+    <div className="relative h-full w-full ">
+      <img src="/log.webp" alt="" className=" absolute h-full w-full" />
+      
+      <div className="h-full px-4 flex flex-col items-center justify-center absolute w-full  bg-[#0000006c] z-20 ">
+        <p className="text-4xl font-extrabold font-serif text-white"> Welcome Back</p>
+        <p className="text-white text-lg px-3 font-mono font-semibold text-center py-3">{`Please ${showLogin?"log in":"sign up"} using your personal information to stay connected with us...`}</p>
+      </div>
       </div>
     </>
   );
--- src/pages/Authentication.tsx
+++ src/pages/Authentication.tsx
@@ -14,26 +14,24 @@ function Authentication() {
           className="absolute   h-full w-full  blur-md"
         />
 
-        <div className="grid grid-cols-4 w-[800px] h-[450px] ">
-          {showLogin ? (
-            <>
-              <div className="col-span-2  bg-white relative   w-100 ">
-                <Welcome />
-              </div>
-              <div className="col-span-2 relative flex items-center justify-center bg-white">
-                <Login setShowLogin={setShowLogin} showLogin={showLogin} />
-              </div>
-            </>
-          ) : (
-            <>
-              <div className="col-span-2  bg-white relative  w-100  py-3">
-                <Register setShowLogin={setShowLogin} showLogin={showLogin} />
-              </div>
-              <div className="col-span-2 relative flex items-center justify-center bg-white">
-                <Welcome />
-              </div>
-            </>
-          )}
+        <div className="grid grid-cols-4 relative w-[800px] h-[450px] ">
+          <div
+            className={`w-[400px] ${
+              showLogin ? "left-0" : "right-0"
+            } z-30 h-full absolute  transition-all duration-500 ease-in-out`}
+            style={{
+              left: 0,
+              transform: showLogin ? "translateX(0)" : "translateX(100%)",
+            }}
+          >
+            <Welcome showLogin={showLogin} />
+          </div>
+          <div className="col-span-2 w-full bg-white ">
+            <Register setShowLogin={setShowLogin} showLogin={showLogin} />
+          </div>
+          <div className="col-span-2 relative flex items-center justify-center bg-white">
+            <Login setShowLogin={setShowLogin} showLogin={showLogin} />
+          </div>
         </div>
       </div>
     </>
